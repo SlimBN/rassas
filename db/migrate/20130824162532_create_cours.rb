f1df679e960a4c6e6bae963b4a9fbce5da7a586b
slimBN <elliji@yopmail.com>
2013-09-13 10:04:42 +0100

    name my dick

--- db/migrate/20130824162532_create_cours.rb
+++ db/migrate/20130824162532_create_cours.rb
@@ -1,7 +1,7 @@
 class CreateCours < ActiveRecord::Migration
   def change
     create_table :cours do |t|
-      t.string :nom
+      t.string :name
       t.string :slug
       t.integer :professeur_id
       t.integer :matiere_id
